0222021dae4bfe95bb4e71e1f6ca7569ba9f6fb3
Amy Zhu <szhu0793@uni.sydney.edu.au>
2025-04-13 14:40:14 +1000

    0

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+module.exports = withNextra()
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-})
+});
 
-module.exports = withNextra()
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_STRATA: process.env.NEXT_PUBLIC_STRATA,
+  },
+};
+
+module.exports = withNextra(nextConfig);

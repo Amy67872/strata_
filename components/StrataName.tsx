6138346fe928775d448bf719c9e35f595b75a378
Amy Zhu <szhu0793@uni.sydney.edu.au>
2025-04-13 16:41:41 +1000

    1

--- components/StrataName.tsx
+++
@@ -1,6 +0,0 @@
-const StrataName = () => {
-    return <span>{process.env.NEXT_PUBLIC_STRATA || "Default Strata"}</span>;
-  };
-  
-  export default StrataName;
-  
--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-import withNextra from 'nextra'
-
-const config = withNextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx',
-})
-
-export default config
